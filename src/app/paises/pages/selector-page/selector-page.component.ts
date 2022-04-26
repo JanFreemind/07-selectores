fc34b63dbd3105e81ce22280b36e4ac9f864dbb2
JanPLosa <jplosa@tlsi.es>
2022-04-26 12:29:40 +0200

    Limpiar pais cuando el primer selector cambia

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { switchMap, tap } from 'rxjs/operators';
+
 import { PaisesService } from '../../services/paises.service';
 import { PaisSmall } from '../../interfaces/paises.interface';
 
@@ -29,15 +32,27 @@ export class SelectorPageComponent implements OnInit {
     this.regiones = this.paisesService.regiones;
 
     // cuando cambie la region 
-    this.miFormulario.get('region')?.valueChanges
-      .subscribe( region => {
-        console.log(region);
+    //Primera aproximacion
+    //this.miFormulario.get('region')?.valueChanges
+    //  .subscribe( region => {
+    //    console.log(region);
 
-        this.paisesService.getPaisesPorRegion( region )
-          .subscribe( paises => {
-            console.log( paises );
-            this.paises = paises;
-          })
+    //    this.paisesService.getPaisesPorRegion( region )
+    //      .subscribe( paises => {
+    //        console.log( paises );
+    //        this.paises = paises;
+    //      })
+    //  })
+
+    this.miFormulario.get('region')?.valueChanges
+      .pipe(
+        tap( ( _ ) => {
+          this.miFormulario.get('pais')?.reset('');
+        }),
+        switchMap( region => this.paisesService.getPaisesPorRegion( region ) )
+      )
+      .subscribe( paises => {
+        this.paises = paises;
       })
 
   }
